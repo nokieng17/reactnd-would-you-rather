91740198528527f27003319c207512fbdf2d14b2
nokieng17 <nokieng17@gmail.com>
2019-06-26 14:38:39 +0700

    add theme

---
+++ src/ui/themes/theme.js
@@ -0,0 +1,21 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const palette = {
+    primary: {
+        main: "#2962ff",
+        light: "#5381ff",
+        dark: "#1c44b2",
+        contrastText: "#fff"
+    },
+    secondary: {
+        main: "#43a047",
+        light: "#68b36b",
+        dark: "#2e7031",
+        contrastText: "#fff"
+    }
+
+}
+
+const themeName = 'Blue Ribbon Grenadier Polar Bear';
+
+export default createMuiTheme({ palette, themeName });
